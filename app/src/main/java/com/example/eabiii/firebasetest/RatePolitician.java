8dbd69c9a35a2e890fcb92493127372e1038b3e8
eabiii <edbar_10@yahoo.com>
2018-03-08 00:51:25 +0800

    beta layout for politician view

--- app/src/main/java/com/example/eabiii/firebasetest/RatePolitician.java
+++ app/src/main/java/com/example/eabiii/firebasetest/RatePolitician.java
@@ -1,8 +1,42 @@
 package com.example.eabiii.firebasetest;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by eabiii on 08/03/2018.
  */
 
-public class RatePolitician {
+public class RatePolitician extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private TextView poliName,position,partylist;
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
+    FirebaseAuth mAuth;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+        ratingBar=findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                //get rating
+            }
+        });
+
+
+    }
+
+
+
 }
